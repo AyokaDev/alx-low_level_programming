b7e92cdb5ac3a47dc47081cf5e109d07c6539597
AyokaDev <ayokateki@gmail.com>
2022-09-11 10:38:53 +0000

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+ * main - Entry point
  *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void);
 {
