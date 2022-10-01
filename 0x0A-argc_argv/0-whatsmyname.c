25a4e9e3f02d8a214541c26623ae76670f51cee6
AyokaDev <ayokateki@gmail.com>
2022-10-01 21:11:21 +0000

    Silence is argument carried out by

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * write a program that prints its name
- *
- * Return: Always 0 (Success)
- *
- */
+* main -> A program that prints the name followed by a new line
+* Return: an integer
+* @argc: number of cli arguments
+* @argv: a Vector that stores the cli arguments.
+*/
+
 int main(int argc, char **argv)
 {
 	(void)argc;
-
 	printf("%s\n", *argv);
 	return (0);
 }
