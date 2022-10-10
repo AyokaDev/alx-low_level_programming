7a5f7c401e8a68c59a46b201761fc1c1b2d74ca3
AyokaDev <ayokateki@gmail.com>
2022-10-10 13:55:15 +0000

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - A dog structure
+ *
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Write a dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
