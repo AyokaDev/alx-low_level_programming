0fb6b8c518aee2690ea5c70c3241a6808572d2fb
AyokaDev <ayokateki@gmail.com>
2022-10-12 23:28:21 +0000

    All

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,9 +1,19 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 
 int op_add(int a, int b);
